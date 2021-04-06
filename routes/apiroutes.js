342d3a58f44aa83cdb8dd4f451210ba82444fcd6
Sean Kelly <invertedrdm@gmail.com>
2021-04-06 01:08:36 -0400

    added id number to notes as they are added

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,8 +3,9 @@ const userNotes = require('../db/db');
 module.exports = (app) => {
     app.get('/api/notes', (req, res) => res.json(userNotes));
 
-    // app.post('/api/notes', (req, res) => {
-    //         console.log("test");
-    //         userNotes.push(req.body);
-    //   });
+    app.post('/api/notes', (req, res) => {
+            req.body.id = (userNotes.length+1);
+            console.log(req.body.id);
+            userNotes.push(req.body);
+      });
 }
